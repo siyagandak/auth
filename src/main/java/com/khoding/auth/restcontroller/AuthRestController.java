2c4d09ecdce657ab03cd20c663c616e47a035311
quedah <siyagandak@gmail.com>
2022-01-07 16:13:46 +0200

    Add signUpAdmin end point

--- src/main/java/com/khoding/auth/restcontroller/AuthRestController.java
+++ src/main/java/com/khoding/auth/restcontroller/AuthRestController.java
@@ -1,9 +1,6 @@
 package com.khoding.auth.restcontroller;
 
-import com.khoding.auth.domain.login.Role;
 import com.khoding.auth.domain.login.User;
-import com.khoding.auth.domain.login.UserRole;
-import com.khoding.auth.repository.RoleRepository;
 import com.khoding.auth.repository.UserRepository;
 import com.khoding.auth.response.JwtResponse;
 import com.khoding.auth.response.MessageResponse;
@@ -13,9 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,46 +17,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthRestController {
     private final SignupService signupService;
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtUtils jwtUtils;
-    private final AuthenticationManager authenticationManager;
-
     private final static String LOGGER_PREFIX = "[Auth Controller]";
     private final static Logger LOGGER = LoggerFactory.getLogger(AuthRestController.class);
 
-    public AuthRestController(SignupService signupService, UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtils jwtUtils,
-                              AuthenticationManager authenticationManager) {
+    public AuthRestController(SignupService signupService) {
         this.signupService = signupService;
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtUtils = jwtUtils;
-        this.authenticationManager = authenticationManager;
     }
 
-    @PostMapping("/signup")
-    public ResponseEntity<?> signUpUser(@RequestBody @Valid SignUpRequest signUpRequest) {
-        LOGGER.info("{} SignUp request... {}", LOGGER_PREFIX, signUpRequest);
-        if (signupService.checkUserExits(signUpRequest.getUsername())) {
-            LOGGER.info("{} Username {} exits => {}", LOGGER_PREFIX, signUpRequest.getUsername(),
-                    signupService.signUpUser(signUpRequest));
+    @PostMapping("/signup/user")
+    public ResponseEntity<?> signUpUser(@RequestBody @Valid UserSignUpRequest userSignUpRequest) {
+        LOGGER.info("{} SignUp request... {}", LOGGER_PREFIX, userSignUpRequest);
+        if (signupService.checkUserExits(userSignUpRequest.getMobileNumber())) {
+            LOGGER.info("{} Username {} exits => {}", LOGGER_PREFIX, userSignUpRequest.getMobileNumber(),
+                    signupService.signUpUser(userSignUpRequest));
             return ResponseEntity
                     .badRequest()
                     .body(MessageResponse.buildMessage("Error: Username already exists"));
         }
-        User user = signupService.signUpUser(signUpRequest);
+        User user = signupService.signUpUser(userSignUpRequest);
         return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/signup/admin")
+    public ResponseEntity<?> signUpAdmin(@RequestBody @Valid AdminSignUpRequest adminSignUpRequest) {
+        return null;
     }
 
     @PostMapping("signin")
