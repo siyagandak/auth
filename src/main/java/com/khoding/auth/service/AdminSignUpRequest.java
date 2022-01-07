ab5ac49af69dced299bac3ea23228b809ddc193d
quedah <siyagandak@gmail.com>
2022-01-07 16:04:00 +0200

    Add Admin signup request

---
+++ src/main/java/com/khoding/auth/service/AdminSignUpRequest.java
@@ -0,0 +1,51 @@
+package com.khoding.auth.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+public class AdminSignUpRequest {
+    @Email
+    @NotBlank
+    private String username;
+    @Size(max = 40)
+    @JsonIgnore
+    @NotBlank
+    private String password;
+    private Set<String> role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "AdminSignUpRequest{" +
+                "username='" + username + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
