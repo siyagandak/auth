c354f6ab462b3047d0a1f9ae236766cca65d98a0
quedah <siyagandak@gmail.com>
2022-01-10 09:50:50 +0200

    Add adminLoginRequest

---
+++ src/main/java/com/khoding/auth/service/AdminLoginRequest.java
@@ -0,0 +1,33 @@
+package com.khoding.auth.service;
+
+import javax.validation.constraints.NotBlank;
+
+public class AdminLoginRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String pin;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    @Override
+    public String toString() {
+        return "AdminLoginRequest{" +
+                "username='" + username + '\'' +
+                '}';
+    }
+}
